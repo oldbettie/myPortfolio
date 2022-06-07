0525ed50520d430bd381a5b96843cedc13b0c61e
oldbettiegaming@gmail.com <oldbettiegaming@gmail.com>
2022-06-07 12:06:20 +0800

    end of course update

--- components/Project.js
+++ components/Project.js
@@ -1,22 +1,28 @@
 import Btn from "./Btn";
 import styles from "../styles/Home.module.scss";
 import { FaGithub } from "react-icons/fa";
+import { useState } from "react";
 
 function Project({ content }) {
-	console.log(content);
+	const [active, setActive] = useState(false);
 	return (
 		<div className={styles.projectCard}>
 			<div className={styles.directLinks}>
-				<a href={content.website}>
-					<Btn content={content.btn} />
-				</a>
+				<div className={styles.leftBtns}>
+					<a href={content.website}>
+						<Btn content={content.btn} />
+					</a>
+					<Btn content={"Info"} onClick={() => setActive(!active)} />
+				</div>
 				<a href={content.github} className={`${styles.faIcon} btnLink`}>
 					<FaGithub />
 				</a>
 			</div>
 			<div className={styles.cardContent}>
-				<p>{content.about}</p>
-				<img src={content.images.src} alt="" />
+				{active && <p>{content.about}</p>}
+				<a href={content.website}>
+					<img src={content.images.src} alt="" />
+				</a>
 				{/* <Image src={content.images.src} width="400px" height="300px" /> */}
 			</div>
 		</div>
