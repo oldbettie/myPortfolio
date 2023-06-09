966b600bab4190d2455d14a27b23c3315bbaf511
jjeaton <oldbettiegaming@gmail.com>
2023-06-09 09:50:32 +0800

    add checker script and run

--- components/About.js
+++ components/About.js
@@ -13,12 +13,12 @@ function About() {
           solution but also how to do it as efficiently as possible.
         </p>
         <p>
-          I consider myself a &quot;maker&quot; someone that loves building and creation,
-          I could be building the next big app, a game, designing some new handy
-          3d printed components. I once built from the ground up a van life van
-          with a dream tech setup that I lived in while I traveled America for a
-          year. I am someone that finds value in learning and evolving even if
-          it&apos;s the small things.
+          I consider myself a &quot;maker&quot; someone that loves building and
+          creation, I could be building the next big app, a game, designing some
+          new handy 3d printed components. I once built from the ground up a van
+          life van with a dream tech setup that I lived in while I traveled
+          America for a year. I am someone that finds value in learning and
+          evolving even if it&apos;s the small things.
         </p>
         <p>
           My life experiences have taught me to keep my composure in tough
