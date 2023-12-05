74e58251ef16286d62ceccf68029faa919f889a2
oldbettie <oldbettiegaming@gmail.com>
2023-12-05 08:43:07 +0800

    fix footer spacing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
-    <div className="md:h-[300px] md:space-y-10 my-auto py-10 font-console w-full text-font_main md:text-[30px] text-center md:text-left mx-auto">
-      <h3 className="w-[320px]">Let's build something amazing together.</h3>
+    <div className=" md:space-y-5 m-auto py-10 font-console  text-font_main md:text-[30px] text-center lg:text-left">
+      <h3>Let's build something amazing together.</h3>
       <h3>
         JJ Eaton -{" "}
         <a className="underline" href="mailto: jayleaton@gmail.com">
